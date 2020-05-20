947bd7419e0603f9d438013ae6b6f2adca2d4270
HGollnick <57234132+HGollnick@users.noreply.github.com>
2020-05-20 09:58:34 -0300

    onNavItemClick bug correction

--- app-webclient/index.js
+++ app-webclient/index.js
@@ -1,55 +1,21 @@
+let routes = {
+  '/': homepage,
+  '/index.html': homepage,
+  '/todo': todoController,
+  '/post': postController,
+  '/contact': contact,
+};
+
+let contentDiv = document.getElementById('content');
+
 if (localStorage.getItem('token') != null) {
 
   document.body.innerHTML = getTemplate();
-  let contentDiv = document.getElementById('content');
 
   loadCSS('styles.css');
 
-  let routes = {
-    '/': homepage,
-    '/index.html': homepage,
-    '/todo': todoController,
-    '/post': postController,
-    '/contact': contact,
-  };
-
   window.onpopstate = () => {
     contentDiv.innerHTML = routes[window.location.pathname];
-  }
-
-  onNavItemClick = (pathName) => {
-    if (localStorage.getItem('token') != null) {
-      window.history.pushState({}, pathName, window.location.origin + pathName);
-
-      if (typeof routes[pathName] === 'string' || routes[pathName] instanceof String) {
-        contentDiv.innerHTML = routes[pathName];
-      } else {
-        const currentRoute = pathName.split('/')[1];
-        const pathRoute = '/' + currentRoute;
-        const pathParam = pathName.split('/')[2];
-
-        let dashboard = document.getElementById('dashboard');
-        let todo = document.getElementById('todo');
-        let post = document.getElementById('post');
-        let contact = document.getElementById('contact');
-        let files = document.getElementById('files');
-
-        dashboard.classList.remove("active");
-        todo.classList.remove("active");
-        post.classList.remove("active");
-        contact.classList.remove("active");
-        files.classList.remove("active");
-
-        loadCSS('app/' + currentRoute + pathRoute + '.styles.css');
-
-        let element = document.getElementById(currentRoute);
-        element.classList.add("active");
-        contentDiv.innerHTML = routes[pathRoute].getView().getTemplate();
-        routes[pathRoute].onInit(pathParam);
-      }
-    } else {
-      alert('User unauthorized');
-    }
   }
 
   contentDiv.innerHTML = routes[window.location.pathname];
@@ -58,6 +24,44 @@ if (localStorage.getItem('token') != null) {
   loadCSS('login/login.styles.css');
   loginController.onInit();
   document.body.innerHTML = loginController.getView().getTemplate();
+}
+
+onNavItemClick = (pathName) => {
+  if (localStorage.getItem('token') != null) {
+
+    contentDiv = document.getElementById('content');
+
+    window.history.pushState({}, pathName, window.location.origin + pathName);
+
+    if (typeof routes[pathName] === 'string' || routes[pathName] instanceof String) {
+      contentDiv.innerHTML = routes[pathName];
+    } else {
+      const currentRoute = pathName.split('/')[1];
+      const pathRoute = '/' + currentRoute;
+      const pathParam = pathName.split('/')[2];
+
+      let dashboard = document.getElementById('dashboard');
+      let todo = document.getElementById('todo');
+      let post = document.getElementById('post');
+      let contact = document.getElementById('contact');
+      let files = document.getElementById('files');
+
+      dashboard.classList.remove("active");
+      todo.classList.remove("active");
+      post.classList.remove("active");
+      contact.classList.remove("active");
+      files.classList.remove("active");
+
+      loadCSS('app/' + currentRoute + pathRoute + '.styles.css');
+
+      let element = document.getElementById(currentRoute);
+      element.classList.add("active");
+      contentDiv.innerHTML = routes[pathRoute].getView().getTemplate();
+      routes[pathRoute].onInit(pathParam);
+    }
+  } else {
+    alert('User unauthorized');
+  }
 }
 
 logout = () => {
